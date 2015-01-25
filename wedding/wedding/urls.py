659e9dc16de70896a7c76eddb1842ff4b5403784
Thibaut Carlier <thibaut@critizr.com>
2015-01-25 17:02:42 +0100

    add robots.txt

--- wedding/wedding/urls.py
+++ wedding/wedding/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.views.generic.base import TemplateView
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'wedding.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+ 
+    (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt'))
 )
