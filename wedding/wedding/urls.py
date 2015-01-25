abd59cb938b98fb566017b0f2b6b79615eb3f67a
Thibaut Carlier <thibaut@critizr.com>
2015-01-25 17:08:29 +0100

    settings

--- wedding/wedding/urls.py
+++ wedding/wedding/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'wedding.views.home', name='home'),
@@ -9,4 +12,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
  
     (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt'))
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
